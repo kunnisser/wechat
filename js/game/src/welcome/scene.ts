1a1acd256b163c6043b5cff0c4f79ff682636bfe
kunnisser <kunnisser@sina.cn>
2023-09-05 23:37:47 +0800

    v4版本container不支持angle

--- js/game/src/welcome/scene.ts
+++ js/game/src/welcome/scene.ts
@@ -2,15 +2,14 @@
  * @Author: kunnisser
  * @Date: 2021-02-26 14:50:22
  * @LastEditors: kunnisser
- * @LastEditTime: 2023-09-05 17:38:22
- * @FilePath: /wechat/js/game/src/welcome/scene.ts
+ * @LastEditTime: 2023-09-05 23:34:18
+ * @FilePath: \wechat\js\game\src\welcome\scene.ts
  * @Description: ---- 示例欢迎场景 ----
  */
-import KnScene from "ts@/kuni/lib/gameobjects/kn_scene";
-import Game from "ts@/kuni/lib/core";
-import { utils } from "ts@/kuni/pixi";
-import KnSprite from "ts@/kuni/lib/gameobjects/kn_sprite";
-import KnGroup from "ts@/kuni/lib/gameobjects/kn_group";
+import KnScene from 'ts@/kuni/lib/gameobjects/kn_scene';
+import Game from 'ts@/kuni/lib/core';
+import KnSprite from 'ts@/kuni/lib/gameobjects/kn_sprite';
+import KnGroup from 'ts@/kuni/lib/gameobjects/kn_group';
 
 class Welcome extends KnScene {
   game: Game;
@@ -23,52 +22,53 @@ class Welcome extends KnScene {
     this.game = game;
     this.key = key;
     this.resources = {
-      gameBg: "assets/images/gameBg.png",
-      rocket: "assets/images/rocket.png",
-      fire: "assets/atlas/fire.json",
-      moon: "assets/images/moon.png"
+      gameBg: 'assets/images/gameBg.png',
+      rocket: 'assets/images/rocket.png',
+      fire: 'assets/atlas/fire.json',
+      moon: 'assets/images/moon.png',
     };
   }
 
   boot() {}
 
   create() {
-    const bg: KnSprite = this.game.add.background("bg", "gameBg");
+    const bg: KnSprite = this.game.add.background('bg', 'gameBg');
     bg.visible = true;
     this.addChild(bg);
-    this.moonGroup = this.game.add.group("planet", this);
+    this.moonGroup = this.game.add.group('planet', this);
     this.moonGroup.x = this.game.config.half_w;
     this.moonGroup.y = this.game.config.half_h;
-    const moon = this.game.add.image("moon", "moon", this.moonGroup);
+
+    const moon = this.game.add.image('moon', 'moon', this.moonGroup);
     moon.anchor.set(0.5, 0.5);
     moon.x = 0;
     moon.y = 0;
     this.moon = moon;
     const rocket: KnSprite = this.game.add.image(
-      "rocket",
-      "rocket",
+      'rocket',
+      'rocket',
       this.moonGroup
     );
     rocket.anchor.set(0.5, 1);
     rocket.x = moon.x;
     rocket.y = moon.y - moon.height * 0.5;
-    const fire = this.game.add.animation(
-      ["fire1.png", "fire2.png", "fire3.png"].map(
-        (key) => utils.TextureCache[key]
-      ),
-      0.4
-    );
-    fire.anchor.set(0.5);
-    fire.scale.set(0.55);
-    fire.position.set(rocket.x, rocket.y + rocket.height - 10);
-    fire.angle = 180;
-    this.addChild(fire);
-    fire.visible = false;
-    fire.play();
+    // const fire = this.game.add.animation(
+    //   ['fire1.png', 'fire2.png', 'fire3.png'].map(
+    //     (key) => utils.TextureCache[key]
+    //   ),
+    //   0.4
+    // );
+    // fire.anchor.set(0.5);
+    // fire.scale.set(0.55);
+    // fire.position.set(rocket.x, rocket.y + rocket.height - 10);
+    // fire.angle = 180;
+    // this.addChild(fire);
+    // fire.visible = false;
+    // fire.play();
   }
 
   update() {
-    this.moonGroup.angle += 1;
+    this.moonGroup.rotation += Math.PI / 180;
   }
 
   reset() {
