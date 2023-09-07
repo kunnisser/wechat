fa08d27eb24531ea3533bf36c263912c6a78831f
kunnisser <kunnisser@sina.cn>
2023-09-07 23:06:30 +0800

    dragonBones去除export.的commonjs模块代码，防止和ESM模块冲突

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,52 @@
-const path = require("path");
-const webpack = require("webpack");
-const progressBarPlugin = require("progress-bar-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const progressBarPlugin = require('progress-bar-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   watch: true,
-  devtool: "cheap-source-map",
-  entry: path.resolve("./", "js/game/main.ts"),
+  devtool: 'cheap-source-map',
+  entry: path.resolve('./', 'js/game/main.ts'),
   output: {
-    path: path.resolve("./", "dist"),
-    filename: "main.min.js",
-    library: "WXMiniGameTs",
-    libraryTarget: "umd",
-    libraryExport: "default",
-    globalObject: "this"
+    path: path.resolve('./', 'dist'),
+    filename: 'main.min.js',
+    library: 'WXMiniGameTs',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    globalObject: 'this',
   },
   module: {
     rules: [
       {
         test: /(.ts)$/,
         use: {
-          loader: "ts-loader"
-        }
+          loader: 'ts-loader',
+        },
       },
       {
         test: /(.js)$/,
         use: [
           {
-            loader: "babel-loader"
-          }
-        ]
-      }
-    ]
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
   },
   plugins: [new progressBarPlugin()],
   resolve: {
     fallback: {
-      path: require.resolve("path-browserify"),
-      url: require.resolve("url/")
+      path: require.resolve('path-browserify'),
+      url: require.resolve('url/'),
     },
     alias: {
-      "pixi.js": path.resolve(__dirname, "core/pixi.js"),
-      "ts@/kuni": path.resolve(__dirname, "js/game")
+      'pixi.js': path.resolve(__dirname, 'core/pixi.js'),
+      'dragonBones.js': path.resolve(__dirname, 'core/dragonBones.min.js'),
+      'ts@/kuni': path.resolve(__dirname, 'js/game'),
     },
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.tsx', '.ts', '.js'],
   },
   optimization: {
-    minimize: true
-  }
+    minimize: true,
+  },
 };
